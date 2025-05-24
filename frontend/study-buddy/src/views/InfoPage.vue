af55f370c477bfd5b69ea6fc8f8ed280763d66a1
Viktoria Paschinger <viktoria.paschinger@it-experience.at>
2025-05-24 13:17:10 +0200

    Added Welcompage as Infopage as one of the tabs

---
+++ frontend/study-buddy/src/views/InfoPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Info</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Study Buddy</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <h5>Your personal study assistent</h5>
+      <p>Studdy buddy helps millions of students manage university courses, track assignments, organize tasks, and meet deadlines. 
+        The app lets you create and organize assignments per course and track completion.</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+</script>
