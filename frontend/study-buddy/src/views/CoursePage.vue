46710ffaac8a03f46bab8ece65f59dbe49dc7de7
Viktoria Paschinger <viktoria.paschinger@it-experience.at>
2025-05-25 21:28:43 +0200

    added single Course Page File not functional

---
+++ frontend/study-buddy/src/views/CoursePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <base-layout page-title="Course MMI">
+    <course-list-item title="MMI"></course-list-item>
+    <div v-if="loading">Loading Assignments…</div>
+    <assignment-list v-else :assignments="assignments"></assignment-list>
+  </base-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AssignmentList from '@/components/AssignmentList.vue';
+import { getAssignments, Assignment } from '@/services/assignmentService'
+
+export default defineComponent({
+  components: {
+    AssignmentList
+  },
+  data() {
+    return {
+      assignments: [] as Assignment[],
+      loading: true
+
+    }
+  },
+  mounted() {
+    getAssignments().then(data => {
+      this.assignments = data
+      this.loading = false
+    })
+  }
+})
+</script>
+
