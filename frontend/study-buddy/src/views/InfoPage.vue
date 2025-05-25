ed535c3e396ef560ebe81ff16683bb5d76d8ff53
Viktoria Paschinger <viktoria.paschinger@it-experience.at>
2025-05-25 13:26:55 +0200

    Fixed styling of InfoPage/Welcomepage

--- frontend/study-buddy/src/views/InfoPage.vue
+++ frontend/study-buddy/src/views/InfoPage.vue
@@ -8,12 +8,15 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Study Buddy</ion-title>
+          <ion-title size="large">Info</ion-title>
         </ion-toolbar>
       </ion-header>
-      <h5>Your personal study assistent</h5>
-      <p>Studdy buddy helps millions of students manage university courses, track assignments, organize tasks, and meet deadlines. 
+      <div class="ion-padding">
+        <h2>Study Buddy</h2>
+        <h5>Your personal study assistent</h5>
+        <p>Studdy buddy helps millions of students manage university courses, track assignments, organize tasks, and meet deadlines. 
         The app lets you create and organize assignments per course and track completion.</p>
+      </div>
     </ion-content>
   </ion-page>
 </template>
