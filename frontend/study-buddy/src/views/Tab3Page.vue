5002d3c1252f3f3622791388a24ee7a871edcac3
Viktoria Paschinger <viktoria.paschinger@it-experience.at>
2025-05-25 18:15:46 +0200

    removed Explore Container

--- frontend/study-buddy/src/views/Tab3Page.vue
+++ frontend/study-buddy/src/views/Tab3Page.vue
@@ -12,12 +12,11 @@
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 3 page" />
+      <p>Empty</p>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
