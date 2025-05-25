e97aef301025dc57aea174acb25a4b2254e1e7ac
Viktoria Paschinger <viktoria.paschinger@it-experience.at>
2025-05-25 20:42:12 +0200

    added BaseLayout Component

--- frontend/study-buddy/src/main.ts
+++ frontend/study-buddy/src/main.ts
@@ -34,9 +34,13 @@ import '@ionic/vue/css/palettes/dark.system.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import BaseLayout from './components/BaseLayout.vue';
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
+
+app.component('base-layout', BaseLayout)
 
 router.isReady().then(() => {
   app.mount('#app');
