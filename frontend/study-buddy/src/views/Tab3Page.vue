c68f7940f9236ed703255d1ec127ee8376edcc6f
Viktoria Paschinger <viktoria.paschinger@it-experience.at>
2025-05-26 17:42:30 +0200

    add coursepage to tab 3 for development

--- frontend/study-buddy/src/views/Tab3Page.vue
+++ frontend/study-buddy/src/views/Tab3Page.vue
@@ -1,8 +1,11 @@
 <template>
   <base-layout page-title="Tab3">
     <p>Empty</p>
+    <course-page course='{id = 1}'></course-page>
   </base-layout>
+  
 </template>
 
 <script setup lang="ts">
+import CoursePage from '@/views/CoursePage.vue'
 </script>
