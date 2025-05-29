09d08a3eed5ef54eb6af6aa4b246b5cdd8084777
Viktoria Paschinger <viktoria.paschinger@it-experience.at>
2025-05-29 15:45:07 +0200

    changed vite config to return 500 instead of index

--- frontend/study-buddy/vite.config.ts
+++ frontend/study-buddy/vite.config.ts
@@ -23,6 +23,10 @@ export default defineConfig({
         target: "http://localhost:8080",
         changeOrigin: true,
       },
+      "/users": {
+        target: "http://localhost:8080", 
+        changeOrigin: true,
+      },
     },
   },
   test: {
