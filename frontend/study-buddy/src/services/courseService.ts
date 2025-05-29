a15ba6d3a0939bb7ab3c11ff028e6c2e7bb4f124
kisin1 <se24m047@technikum-wien.at>
2025-05-29 11:53:25 +0200

    Integrated axios int frontend

--- frontend/study-buddy/src/services/courseService.ts
+++ frontend/study-buddy/src/services/courseService.ts
@@ -1,39 +1,42 @@
-import { Assignment } from '@/types/assignment'
-import { Course } from '@/types/course'
-import axios from 'axios'
+import { Assignment } from "@/types/assignment";
+import { Course } from "@/types/course";
+// import axios from 'axios'
+import api from "./api";
 
 export async function getCourses(): Promise<Course[]> {
   try {
-    const response = await axios.get<Course[]>('/courses')
-    return response.data
+    const response = await api.get<Course[]>("/courses");
+    return response.data;
   } catch (error) {
-    console.warn('API /courses failed, returning fallback data.')
-    console.error(error)
+    console.warn("API /courses failed, returning fallback data.");
+    console.error(error);
 
     // Fallback-Daten
     return [
-      { id: 1, title: 'Fallback Course 1', description: 'Some Description' },
-      { id: 2, title: 'Fallback Course 2', description: 'Some Description' },
-      { id: 3, title: 'Fallback Course 3', description: 'Some Description' }
-    ]
+      { id: 1, title: "Fallback Course 1", description: "Some Description" },
+      { id: 2, title: "Fallback Course 2", description: "Some Description" },
+      { id: 3, title: "Fallback Course 3", description: "Some Description" },
+    ];
   }
 }
 
-export async function getAssignmentByCourseId(id: number): Promise<Assignment[]> {
+export async function getAssignmentByCourseId(
+  id: number
+): Promise<Assignment[]> {
   try {
-    const response = await axios.get<Assignment[]>('courses/{id}/assignments', {
-      params: { sort: 'date' }
-    })
-    return response.data
+    const response = await api.get<Assignment[]>("courses/{id}/assignments", {
+      params: { sort: "date" },
+    });
+    return response.data;
   } catch (error) {
-    console.warn('API /assignments failed, returning fallback data.')
-    console.error(error)
+    console.warn("API /assignments failed, returning fallback data.");
+    console.error(error);
 
     // Fallback-Daten
     return [
-      { id: 1, title: 'Fallback Course Assignment 1', date: '2025-06-01' },
-      { id: 2, title: 'Fallback Course Assignment 2', date: '2025-06-05' },
-      { id: 3, title: 'Fallback Course Assignment 3', date: '2025-06-05' }
-    ]
+      { id: 1, title: "Fallback Course Assignment 1", date: "2025-06-01" },
+      { id: 2, title: "Fallback Course Assignment 2", date: "2025-06-05" },
+      { id: 3, title: "Fallback Course Assignment 3", date: "2025-06-05" },
+    ];
   }
 }
--- frontend/study-buddy/vite.config.ts
+++ frontend/study-buddy/vite.config.ts
@@ -1,35 +1,32 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
+  plugins: [vue(), legacy()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     proxy: {
-      '/assignments': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
+      "/assignments": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
       },
-      '/courses': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
+      "/courses": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
   },
   test: {
     globals: true,
-    environment: 'jsdom'
-  }
-})
+    environment: "jsdom",
+  },
+});
